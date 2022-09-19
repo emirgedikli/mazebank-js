68eeb2b175db65baed551d5a2909f3ce4bd9d004
emirgedikli <emirwtaha@gmail.com>
2022-09-19 14:17:15 +0200

    new push

--- src/app.js
+++ src/app.js
@@ -1,10 +1,2 @@
-const loginArea = document.getElementById('login-area'),
-    dashboard = document.getElementById('dashboard'),
-    deposit = document.getElementById('deposit'),
-    withdraw = document.getElementById('withdraw'),
-    balance = document.getElementById('balance'),
-    depositInput = document.getElementById('deposit-input'),
-    withdrawInput = document.getElementById('withdraw-input'),
-    depositBtn = document.getElementById('deposit-btn'),
-    withdrawBtn = document.getElementById('withdraw-btn'),
-    submitBtn = document.getElementById('submit-btn');
+import "./assets/css/styles.css";
+import "@fortawesome/fontawesome-free/js/all";
--- src/components/login.js
+++
@@ -1,4 +0,0 @@
-// submitBtn.addEventListener('click', () => {
-//     loginArea.style.display = 'none';
-//     dashboard.classList.remove('d-none');
-// })
